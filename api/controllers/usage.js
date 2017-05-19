0bed8526d9f79d9d85ca52ecc242d8f9279d1a4a
Steven Taylor <steven.taylor@me.com>
2017-05-19 07:04:37 -0700

    Added historical queries

--- api/controllers/usage.js
+++ api/controllers/usage.js
@@ -6,65 +6,138 @@ module.exports.getUsage = (req, res) => {
 
     let id = req.swagger.params.id.value;
     let range = req.swagger.params.range.value;
-    let start = req.swagger.params.start.value;
+    let end = req.swagger.params.end.value;
 
-    if ( start ){
-        start = start.toUTCString().replace('GMT','');
-        start = new Date( start );
+    if ( end ){
+        end = start.toUTCString().replace('GMT','');
+        end = new Date( end );
     }
 
-    if ( !start )
-        start = new Date();
+    if ( !end )
+        end = new Date();
 
-    getUsageBy[range]( id, start, res );
+    getUsageBy[range]( id, end, res );
 
 };
 
 let getUsageBy = {};
 
-getUsageBy['hour'] = (id, start, res) => {
+getUsageBy['hour'] = (id, end, res) => {
 
-    let ts = moment(start).format('YYYY-MM-DD');
+    let ts1 = moment(end).format('YYYY-MM-DD');
+    let ts2 = moment(end).add(1, 'd').format('YYYY-MM-DD');
 
-    let q = `select t1.hour, 
-                ( max(t1.max_summation_delivered) - min(t1.min_summation_delivered) ) -
-                ( max(t1.max_summation_received) - min(t1.min_summation_received) ) as nem
-                from (
-                    select 
-                    from_unixtime(t2.demand_timestamp, '%H') as hour,
-                    min(t2.summation_delivered) as min_summation_delivered,
-                    max(t2.summation_delivered) as max_summation_delivered,
-                    min(t2.summation_received) as min_summation_received,
-                    max(t2.summation_received) as max_summation_received
-                    from (
-                        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp >= UNIX_TIMESTAMP (DATE('${ts}'))
-                    ) t2
-                    group by hour
-                ) t1 
-                group by t1.hour
-                order by t1.hour asc`;
+    let q = `
+    select
+        from_unixtime(t2.demand_timestamp, "%H") as hour,
+        (max(t2.summation_delivered) - min(t2.summation_delivered)) - (max(t2.summation_received) - min(t2.summation_received)) as kwh
+    from (
+        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp >= UNIX_TIMESTAMP (DATE('${ts1}')) AND demand_timestamp < UNIX_TIMESTAMP (DATE('${ts2}') )
+    ) t2
+    group by hour`;
 
     global.rainforest.getData(q)
-        .then( (devices) => {
-            res.json( { data: devices, result : 'ok'  } );
+        .then( (data) => {
+            let r = { start : ts1, end : ts2, samples : data };
+            res.json( { data: r, result : 'ok'  } );
         })
         .catch( (err) => {
             res.status(500).json( { code: err.code || 0, message: err.message } );
         });
 };
 
-getUsageBy['day'] = (id, start, res) => {
+getUsageBy['day'] = (id, end, res) => {
 
+    let ts1 = moment(end).subtract(1, 'd').format('YYYY-MM-DD');
+    let ts2 = moment(end).format('YYYY-MM-DD');
+
+    let q = `
+    select
+        from_unixtime(t2.demand_timestamp, "%Y-%m-%d") as date,
+        (max(t2.summation_delivered) - min(t2.summation_delivered)) - (max(t2.summation_received) - min(t2.summation_received)) as kwh
+    from (
+        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp > UNIX_TIMESTAMP (DATE('${ts2}')) AND demand_timestamp <= UNIX_TIMESTAMP (DATE('${ts2}') )
+    ) t2
+    group by date`;
+
+    global.rainforest.getData(q)
+        .then( (data) => {
+            let r = { start : ts1, end : ts2, samples : data };
+            res.json( { data: r, result : 'ok'  } );
+        })
+        .catch( (err) => {
+            res.status(500).json( { code: err.code || 0, message: err.message } );
+        });
 };
 
-getUsageBy['week'] = (id, start, res) => {
+getUsageBy['week'] = (id, end, res) => {
 
+    let ts1 = moment(end).subtract(7, 'd').format('YYYY-MM-DD');
+    let ts2 = moment(end).format('YYYY-MM-DD');
+
+    let q = `
+    select
+        from_unixtime(t2.demand_timestamp, "%Y-%m-%d") as date,
+        (max(t2.summation_delivered) - min(t2.summation_delivered)) - (max(t2.summation_received) - min(t2.summation_received)) as kwh
+    from (
+        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp > UNIX_TIMESTAMP (DATE('${ts1}')) AND demand_timestamp <= UNIX_TIMESTAMP (DATE('${ts2}') )
+    ) t2
+    group by date`;
+
+    global.rainforest.getData(q)
+        .then( (data) => {
+            let r = { start : ts1, end : ts2, samples : data };
+            res.json( { data: r, result : 'ok'  } );
+        })
+        .catch( (err) => {
+            res.status(500).json( { code: err.code || 0, message: err.message } );
+        });
 };
 
-getUsageBy['month'] = (id, start, res) => {
+getUsageBy['month'] = (id, end, res) => {
 
+    let ts1 = moment(end).subtract(30, 'd').format('YYYY-MM-DD');
+    let ts2 = moment(end).format('YYYY-MM-DD');
+
+    let q = `
+    select
+        from_unixtime(t2.demand_timestamp, "%Y-%m-%d") as date,
+        (max(t2.summation_delivered) - min(t2.summation_delivered)) - (max(t2.summation_received) - min(t2.summation_received)) as kwh
+    from (
+        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp > UNIX_TIMESTAMP (DATE('${ts1}')) AND demand_timestamp <= UNIX_TIMESTAMP (DATE('${ts2}') )
+    ) t2
+    group by date`;
+
+    global.rainforest.getData(q)
+        .then( (data) => {
+            let r = { start : ts1, end : ts2, samples : data };
+            res.json( { data: r, result : 'ok'  } );
+        })
+        .catch( (err) => {
+            res.status(500).json( { code: err.code || 0, message: err.message } );
+        });
 };
 
-getUsageBy['year'] = (id, start, res) => {
+getUsageBy['year'] = (id, end, res) => {
 
+    let ts1 = moment(end).subtract(365, 'd').format('YYYY-MM-DD');
+    let ts2 = moment(end).format('YYYY-MM-DD');
+
+    let q = `
+    select
+        from_unixtime(t2.demand_timestamp, "%Y-%m") as date,
+        (max(t2.summation_delivered) - min(t2.summation_delivered)) - (max(t2.summation_received) - min(t2.summation_received)) as kwh
+    from (
+        select * from sentinel.samples where mac_id = '${id}' and demand_timestamp > UNIX_TIMESTAMP (DATE('${ts1}')) AND demand_timestamp <= UNIX_TIMESTAMP (DATE('${ts2}') )
+    ) t2
+    group by date`;
+
+    global.rainforest.getData(q)
+        .then( (data) => {
+            let r = { start : ts1, end : ts2, samples : data };
+            res.json( { data: r, result : 'ok'  } );
+        })
+        .catch( (err) => {
+            res.status(500).json( { code: err.code || 0, message: err.message } );
+        });
 };
